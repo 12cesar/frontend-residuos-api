b8309ed41b6f651613822dace4907b66aadae75b
julio cesar calderon galindo <julcescalgalty@gmail.com>
2021-10-20 00:46:24 -0500

    primer avance del frontend administrador

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
+const routes: Routes = [
+  // path: '/dashboard'//
+  
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NopagefoundComponent,
+  },
+  
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
